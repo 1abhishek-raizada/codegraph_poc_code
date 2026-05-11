23173873340e89108a2a029f95bb8a4a62b1a328
1abhishek-raizada <raizadaabhishek@seasiaconnect.com>
2026-05-11 10:51:52 +0530

    third commit

--- first_code.py
+++ first_code.py
@@ -1,5 +1,8 @@
 def hello_world():
     print("Hello world")
     
+def add_numbers(a: int, b: int) -> int:
+    """Adds two numbers and returns the result."""
+    return a + b
 
 print(hello_world)    
